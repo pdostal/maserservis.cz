a2c198cc6615524e99cf976921904f1189f82623
pdostal <git@pdostal.cz>
2014-06-17 06:14:25 +0200

    Style background, opacity and radius

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
   include('header.php');
   include('sidebar.php');
+  if (have_posts()) { while (have_posts()) { the_post();
 ?>
 <section id="main" class="<?php echo get_post_type( get_the_ID() ); ?>">
-<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
   <article>
     <div class="content"><?php the_content(__()); ?></div>
   </article>
-<?php } } ?>
 </section>
 <?php
+  } }
   include('footer.php');
 ?>
